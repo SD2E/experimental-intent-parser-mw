d4ca3794371651bf869e61a2491f7706f07d0208
tramyn <tramy.nguy@gmail.com>
2020-06-16 13:49:17 -0700

    update import for MediaIoBaseUpload

--- intent_parser/accessor/google_accessor.py
+++ intent_parser/accessor/google_accessor.py
@@ -1,4 +1,4 @@
-from apiclient.http import MediaIoBaseUpload
+from googleapiclient.http import  MediaIoBaseUpload
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import AuthorizedSession, Request
